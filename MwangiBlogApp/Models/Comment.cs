1aa6b91a999b4da676b254dcc4a248ff3ad47af3
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-07-18 14:34:56 -0400

    Update Comment.cs
    updating my comments models

--- MwangiBlogApp/Models/Comment.cs
+++ MwangiBlogApp/Models/Comment.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace MwangiBlogApp.Models
 {
@@ -11,11 +12,10 @@ namespace MwangiBlogApp.Models
         public int BlogPostId { get; set; }
         public string AuthorId { get; set; }
         public string Body { get; set; }
-
+        [AllowHtml]
         public DateTimeOffset Created { get; set; }
         public DateTimeOffset? Updated { get; set; }
         public string UpdateReason { get; set; }
-
         //virtual Navigation section
         public virtual BlogPost BlogPost { get; set; }
         public virtual ApplicationUser Author { get; set; }
