e702ab7d0421f270ef69a44847b1c289e8a928e0
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-07-21 22:28:14 -0400

    Create font_awesome.js
    Reving error on the debug window

---
+++ MwangiBlogApp/Scripts/font_awesome.js
@@ -0,0 +1,6 @@
+﻿$fa - font - path: "font-awesome/fonts";
+$fa - font - path: "~font-awesome/fonts";
+$fa - font - path: "./node_modules/font-awesome/fonts";
+
+@import '../node_modules/bootstrap/scss/bootstrap.scss';
+@import '../node_modules/font-awesome/scss/font-awesome';
