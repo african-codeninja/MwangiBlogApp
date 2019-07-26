fb4348127d5ef86885a75a57ec99eade491b045b
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-07-26 08:46:24 -0400

    Create GlobalSuppressions.cs
    trying to fix errors

---
+++ MwangiBlogApp/GlobalSuppressions.cs
@@ -0,0 +1,16 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0037:Use inferred member name", Justification = "<Pending>", Scope = "member", Target = "~M:MwangiBlogApp.Controllers.AccountController.Login(MwangiBlogApp.Models.LoginViewModel,System.String)~System.Threading.Tasks.Task{System.Web.Mvc.ActionResult}")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0037:Use inferred member name", Justification = "<Pending>", Scope = "member", Target = "~M:MwangiBlogApp.Controllers.AccountController.Register(MwangiBlogApp.Models.RegisterViewModel)~System.Threading.Tasks.Task{System.Web.Mvc.ActionResult}")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0037:Use inferred member name", Justification = "<Pending>", Scope = "member", Target = "~M:MwangiBlogApp.Controllers.AccountController.ForgotPassword(MwangiBlogApp.Models.ForgotPasswordViewModel)~System.Threading.Tasks.Task{System.Web.Mvc.ActionResult}")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0037:Use inferred member name", Justification = "<Pending>", Scope = "member", Target = "~M:MwangiBlogApp.Controllers.AccountController.SendCode(MwangiBlogApp.Models.SendCodeViewModel)~System.Threading.Tasks.Task{System.Web.Mvc.ActionResult}")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0037:Use inferred member name", Justification = "<Pending>", Scope = "member", Target = "~M:MwangiBlogApp.Controllers.AccountController.ResendEmailConfirmation(MwangiBlogApp.Models.ForgotPasswordViewModel)~System.Threading.Tasks.Task{System.Web.Mvc.ActionResult}")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:MwangiBlogApp.Controllers.BlogPostsController.db")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:MwangiBlogApp.Controllers.CommentsController.db")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:MwangiBlogApp.Controllers.HomeController.context")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:MwangiBlogApp.Controllers.HomeController.db")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>", Scope = "member", Target = "~M:MwangiBlogApp.Controllers.HomeController.Take(System.Int32)~System.Object")]
