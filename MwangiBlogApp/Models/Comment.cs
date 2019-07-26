49b167fd0b328dd1d5705eb78da4d99599a816c1
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-07-26 08:45:39 -0400

    Update Comment.cs
    implementing Allowhtml to fix TinyMCE

--- MwangiBlogApp/Models/Comment.cs
+++ MwangiBlogApp/Models/Comment.cs
@@ -11,8 +11,9 @@ namespace MwangiBlogApp.Models
         public int Id { get; set; }
         public int BlogPostId { get; set; }
         public string AuthorId { get; set; }
-        public string Body { get; set; }
         [AllowHtml]
+        public string Body { get; set; }
+       
         public DateTimeOffset Created { get; set; }
         public DateTimeOffset? Updated { get; set; }
         public string UpdateReason { get; set; }
