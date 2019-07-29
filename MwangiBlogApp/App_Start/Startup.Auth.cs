5aace1e0e1124cb9a81fcded7a8ac35408d10b87
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-07-29 11:35:29 -0400

    Update Startup.Auth.cs

--- MwangiBlogApp/App_Start/Startup.Auth.cs
+++ MwangiBlogApp/App_Start/Startup.Auth.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using Microsoft.AspNet.Identity;
+﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.Google;
-using Owin;
 using MwangiBlogApp.Models;
+using Owin;
+using System;
 
 namespace MwangiBlogApp
 {
@@ -34,7 +33,7 @@ namespace MwangiBlogApp
                         validateInterval: TimeSpan.FromMinutes(30),
                         regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
                 }
-            });            
+            });
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
 
             // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
