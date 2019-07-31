573a1f081b8cad7691cb44c06fd70d4793a55e85
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-07-31 01:35:24 -0400

    Update BlogPost.cs
    Styling the index page to make it look decent

--- MwangiBlogApp/Models/BlogPost.cs
+++ MwangiBlogApp/Models/BlogPost.cs
@@ -10,17 +10,17 @@ namespace MwangiBlogApp.Models
 
         public int Id { get; set; }
         [Required]
-        [MaxLength(30),MinLength(5)]
+        [MaxLength(30),MinLength(2)]
         [Display(Name = "Title")]
         public string Title { get; set; }
         [Required]
-        [MaxLength(50),MinLength(5)]
+        [MaxLength(50),MinLength(2)]
         [Display(Name = "Shortened Blog Title")]
         public string Abstract { get; set; }
         public string Slug { get; set; }
         [AllowHtml]
         [Required]
-        [MaxLength(300),MinLength(20)]
+        [MaxLength(400),MinLength(2)]
         public string Body { get; set; }
         public string MediaUrl { get; set; }
         public bool Published { get; set; }
