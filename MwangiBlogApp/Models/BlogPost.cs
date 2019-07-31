61408746648f1d4e3ece49c39990b1c1ebcccd60
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-07-30 22:08:14 -0400

    Update BlogPost.cs
    Editing models with using System.ComponentModel.DataAnnotations; to validate user entry

--- MwangiBlogApp/Models/BlogPost.cs
+++ MwangiBlogApp/Models/BlogPost.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Web.Mvc;
 
 namespace MwangiBlogApp.Models
 {
     public class BlogPost
     {
+
         public int Id { get; set; }
+        [Required]
+        [MaxLength(30),MinLength(5)]
+        [Display(Name = "Title")]
         public string Title { get; set; }
+        [Required]
+        [MaxLength(50),MinLength(5)]
+        [Display(Name = "Shortened Blog Title")]
         public string Abstract { get; set; }
         public string Slug { get; set; }
         [AllowHtml]
+        [Required]
+        [MaxLength(300),MinLength(20)]
         public string Body { get; set; }
         public string MediaUrl { get; set; }
         public bool Published { get; set; }
